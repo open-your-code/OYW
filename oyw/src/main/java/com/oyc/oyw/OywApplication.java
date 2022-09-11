16b8625b388f83f416fea6e15a681f175b6a745a
open-your-code <openyourcode@gmail.com>
2022-09-11 15:03:43 +0900

    add project

---
+++ oyw/src/main/java/com/oyc/oyw/OywApplication.java
@@ -0,0 +1,13 @@
+package com.oyc.oyw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OywApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OywApplication.class, args);
+	}
+
+}
---
+++ oyw/src/test/java/com/oyc/oyw/OywApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oyc.oyw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OywApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
